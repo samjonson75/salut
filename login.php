4eaf932d0a405dc9a76d2e3e6e9999457e7c2a6d
Jonathan Baskar <j_aska@concordia.ca>
2017-11-28 09:47:01 -0500

    first commit

---
+++ login.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE>
+<html>
+<head>
+<title>GITHUB</title>
+</head>
+<body>
+
+<form action="index.php" method="post">
+Login : <input type="text" name="login"><br>
+Password:<input type="text" name="pass"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
